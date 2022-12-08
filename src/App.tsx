cc64d01091608711c0b454729c3f95623b0704e7
yterasaka <yuki.terasaka@gmail.com>
2022-12-08 16:05:01 +0100

    update 7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 type Todo = {
   value: string;
@@ -8,15 +8,32 @@ export const App = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  // 新しいTodoを作成
+  const handleOnSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setText("");
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input type="submit" value="Add" onSubmit={(e) => e.preventDefault()} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // <form> タグの中でいったん e.preventDefault() しているのは Enter キー打鍵でページそのものがリロードされてしまうのを防ぐため
+          handleOnSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="Add" onSubmit={handleOnSubmit} />
       </form>
     </div>
   );
