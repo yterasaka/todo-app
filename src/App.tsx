646b0b4644edd9f3cce5ab64512add667db095fa
yterasaka <yuki.terasaka@gmail.com>
2022-12-09 14:40:04 +0100

    update 15.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ type Todo = {
   value: string;
   readonly id: number;
   checked: boolean;
+  removed: boolean;
 };
+
+type Filter = "all" | "checked" | "unchecked" | "removed";
 
 export const App = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>("all");
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
@@ -22,6 +26,7 @@ export const App = () => {
       value: text,
       id: new Date().getTime(),
       checked: false,
+      removed: false,
     };
 
     setTodos([newTodo, ...todos]);
@@ -58,32 +63,83 @@ export const App = () => {
     setTodos(newTodos);
   };
 
+  const handleOnRemove = (id: number, removed: boolean) => {
+    const deepCopy = todos.map((todo) => ({ ...todo }));
+
+    const newTodos = deepCopy.map((todo) => {
+      if (todo.id === id) {
+        todo.removed = !removed;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case "all":
+        return !todo.removed;
+      case "checked":
+        return todo.checked && !todo.removed;
+      case "unchecked":
+        return !todo.checked && !todo.removed;
+      case "removed":
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
+
   return (
     <div>
+      <select
+        defaultValue="all"
+        onChange={(e) => setFilter(e.target.value as Filter)}
+      >
+        <option value="all">All Todos</option>
+        <option value="checked">Completed Todos</option>
+        <option value="unchecked">Current Todos</option>
+        <option value="removed">Removed Todos</option>
+      </select>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleOnSubmit();
         }}
       >
-        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
-        <input type="submit" value="Add" onSubmit={handleOnSubmit} />
+        <input
+          type="text"
+          value={text}
+          disabled={filter === "checked" || filter === "removed"}
+          onChange={(e) => handleOnChange(e)}
+        />
+        <input
+          type="submit"
+          value="Add"
+          disabled={filter === "checked" || filter === "removed"}
+          onSubmit={handleOnSubmit}
+        />
       </form>
       <ul>
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return (
             <li key={todo.id}>
               <input
                 type="checkbox"
+                disabled={todo.removed}
                 checked={todo.checked}
                 onChange={() => handleOnCheck(todo.id, todo.checked)}
               />
               <input
                 type="text"
-                disabled={todo.checked}
+                disabled={todo.checked || todo.removed}
                 value={todo.value}
                 onChange={(e) => handleOnEdit(todo.id, e.target.value)}
               />
+              <button onClick={() => handleOnRemove(todo.id, todo.removed)}>
+                {todo.removed ? "Restore" : "Remove"}
+              </button>
             </li>
           );
         })}
